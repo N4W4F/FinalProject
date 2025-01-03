ce68c6c1d1aeb71276693540c6da553ebd44defb
nawafmuflih <nw4f@outlook.com>
2025-01-03 20:35:19 +0300

    Add: HeavyLogistics package

--- HeavyLogistics/src/main/java/com/example/heavylogistics/Controller/VehicleReviewController.java
+++ HeavyLogistics/src/main/java/com/example/heavylogistics/Controller/VehicleReviewController.java
@@ -1,5 +1,13 @@
 package com.example.heavylogistics.Controller;
 
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/vehicle-Review")
+@RequiredArgsConstructor
 public class VehicleReviewController {
-    
+
 }
